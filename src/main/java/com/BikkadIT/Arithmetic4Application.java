b3ec004e7f0c0c83c3a0e2845421fbaf44e32412
Harsha4151 <hkamble4151@gmail.com>
2023-01-30 19:11:33 +0530

    changes by harsha

---
+++ src/main/java/com/BikkadIT/Arithmetic4Application.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Arithmetic4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Arithmetic4Application.class, args);
+	}
+
+}
